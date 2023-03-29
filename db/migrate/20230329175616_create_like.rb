bc2e8ddd46cfae31064634862e3775c55307a0b2
Dell Pc <mudasir9757@gmail.com>
2023-03-30 00:00:02 +0500

    generate migrations

---
+++ db/migrate/20230329175616_create_like.rb
@@ -0,0 +1,9 @@
+class CreateLike < ActiveRecord::Migration[7.0]
+  def change
+    create_table :likes do |t|
+      t.belongs_to :users, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
