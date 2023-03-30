2168b588edbe16dda017d891065829149045a51a
Dell Pc <mudasir9757@gmail.com>
2023-03-30 15:38:33 +0500

    run migrations to update schema

--- db/migrate/20230329175540_create_post.rb
+++ db/migrate/20230329175540_create_post.rb
@@ -1,10 +1,10 @@
 class CreatePost < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.string :Title
-      t.text :Text
-      t.integer :CommentsCounter
-      t.integer :LikesCounter
+      t.string :title
+      t.text :text
+      t.integer :comment_counter
+      t.integer :like_counter
       t.belongs_to :author, null: false, foreign_key: {to_table: :users}
       t.timestamps
     end
