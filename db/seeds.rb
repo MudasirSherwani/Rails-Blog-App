cda0d2b9a0a205f681ed85082f282d5a02e19efb
Dell Pc <mudasir9757@gmail.com>
2023-03-30 15:38:57 +0500

    run db:seed to create data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+   first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+   second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+   
+   first_post = Post.create(author: first_user, title: 'Post Written by Tom', text: 'This is Tom first post')
+   second_post = Post.create(author: first_user, title: 'Post Written by Tom', text: 'This is Tom second post')
+   third_post = Post.create(author: second_user, title: 'Post Written by Lilly', text: 'This is Lilly first post')
+   fourth_post = Post.create(author: second_user, title: 'Post Written by Lilly', text: 'This is Lilly second post')
+
+   Comment.create(posts: first_post, users: first_user, text: 'Hi Tom! 1' )
+   Comment.create(posts: first_post, users: first_user, text: 'Hi Tom! 2' )
+   Comment.create(posts: first_post, users: first_user, text: 'Hi Tom! 3' )
+   Comment.create(posts: second_post, users: first_user, text: 'Hi Tom! 4' )
+   Comment.create(posts: third_post, users: second_user, text: 'Hi Lilly! 1' )
+   Comment.create(posts: fourth_post, users: second_user, text: 'Hi Lilly! 2' )
+   Comment.create(posts: fourth_post, users: second_user, text: 'Hi Lilly! 3' )
