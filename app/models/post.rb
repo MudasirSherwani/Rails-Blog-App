dee222215982e24e195553f8a6fef00e6ce55b04
Dell Pc <mudasir9757@gmail.com>
2023-03-30 16:22:46 +0500

    Add custom methods to model classes

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: 'posts_id'
   has_many :likes, foreign_key: 'posts_id'
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  def update_posts_counter
+    author.increment!(:post_counter)
+  end
+
+  def most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
