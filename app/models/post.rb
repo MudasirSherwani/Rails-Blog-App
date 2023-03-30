e3cf64b373367179b465724d54c9a9ec8e02cc66
Dell Pc <mudasir9757@gmail.com>
2023-03-30 21:33:40 +0500

    Fix linters Error 1

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 250 }
   validates :comment_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :like_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  
+
   def update_posts_counter
     author.increment!(:post_counter)
   end
