e77c3edc5c4513ce8916bda3e3f42a8a28a78eee
Dell Pc <mudasir9757@gmail.com>
2023-03-30 15:37:40 +0500

    changes in model class

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
-  belongs_to :users, class_name: 'User', foreign_key: 'user_id'
-  belongs_to :posts, class_name: 'Post', foreign_key: 'post_id'
+  belongs_to :users, class_name: 'User', foreign_key: 'users_id'
+  belongs_to :posts, class_name: 'Post', foreign_key: 'posts_id'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :likes, foreign_key: 'user_id'
-  has_many :comments, foreign_key: 'user_id'
+  has_many :likes, foreign_key: 'users_id'
+  has_many :comments, foreign_key: 'users_id'
   has_many :posts, foreign_key: 'author_id'
 end
