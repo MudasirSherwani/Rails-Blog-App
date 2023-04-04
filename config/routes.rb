25d0bd4f17eedadfd3fcc7d50a72b00511bbedea
Dell Pc <mudasir9757@gmail.com>
2023-04-04 16:01:56 +0500

    Required Changes done in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :users do
-    resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
-  end
+  end   
 end
