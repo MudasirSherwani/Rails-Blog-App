091d66755db35f2530fff5cbdd0f66035190ae9b
Dell Pc <mudasir9757@gmail.com>
2023-04-04 15:13:03 +0500

    rails generate controller and add routes resources

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
+    def index
+      @users = User.all
+    end
+  
+    def show; end
   end
-
-  def show; end
-end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  get '/users/:user_id/post/:id', to: 'posts#show', as: 'user_post'
-  get '/users', to: 'users#index', as: 'users'
-  get '/users/:id', to: 'users#show', as: 'user'  
+  resources :users do
+    resources :users, only: [:index, :show]
+    resources :posts, only: [:index, :show]
+  end
 end
