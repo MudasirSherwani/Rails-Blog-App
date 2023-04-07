7c318961f6c851509a0810d9c397a0716622801b
Dell Pc <mudasir9757@gmail.com>
2023-04-08 01:21:01 +0500

    Add create method to likesController

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,13 @@
+class LikesController < ApplicationController
+    def create
+      author = params[:user_id]
+      post = params[:post_id]
+      like = Like.new(author_id: author, posts_id: post)
+  
+      if like.save
+        redirect_to user_post_path(author, post)
+      else
+        flash.now[:error] = 'Error Occur During Like Create !'
+      end
+    end
+  end
