d3fa75a2ff169c646760ed831fc640543a15ac0a
Dell Pc <mudasir9757@gmail.com>
2023-04-07 11:24:08 +0500

    Add post counter update after_save

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,7 @@ class Post < ApplicationRecord
   validates :comment_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :like_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  after_save :update_posts_counter
   def update_posts_counter
     author.increment!(:post_counter)
   end
