0def34ee03b3d133846d0747f7db9b2ab0f1fe0a
Dell Pc <mudasir9757@gmail.com>
2023-04-08 01:21:28 +0500

    Add create method to PostController.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,10 +5,26 @@ class PostsController < ApplicationController
     @posts = @user.posts.order(created_at: :desc)
   end
 
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :text))
+    @post.comment_counter = 0
+    @post.like_counter = 0
+    @post.author = current_user
+    if @post.save
+      redirect_to user_posts_path(current_user)
+    else
+      flash[:alert] = "Error Occur During Post Create !"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
+
   def show
     user_id = params[:user_id]
     post_id = params[:id]
-
     @user = User.find(user_id)
     @post = @user.posts.find(post_id)
     @comments = @post.comments.order(created_at: :desc)
