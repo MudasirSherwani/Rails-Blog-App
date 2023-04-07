3fcda542f065e86b18d62a6130e0c08314f7edd1
Dell Pc <mudasir9757@gmail.com>
2023-04-07 11:24:35 +0500

    Add like counter update after_save

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,7 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :posts, class_name: 'Post', foreign_key: 'posts_id'
 
+  after_save :update_like_counter
   def update_like_counter
     posts.increment!(:like_counter)
   end
