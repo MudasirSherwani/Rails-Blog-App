37a24cbef2de873e27d1631b7c6c60d189f8e079
Dell Pc <mudasir9757@gmail.com>
2023-04-12 21:35:09 +0500

    Add redirect test to user

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -1,25 +1,28 @@
 require 'rails_helper'
 require 'spec_helper'
-RSpec.describe 'Users Index', type: :request do
-  subject { page }
-  before(:each) do
-    User.create(name: 'Tom', post_counter: 5, photo: 'image.jpg')
-    User.create(name: 'Lilly', post_counter: 2, id: 12, photo: 'image.jpg')
+RSpec.describe 'Users Index', type: :feature do
+  before(:example) do
+    @user1 = User.create(name: 'Tom', post_counter: 5, photo: 'image.jpg')
+    @user2 = User.create(name: 'Lilly', post_counter: 2, id: 12, photo: 'image.jpg')
+    visit users_path
+  end
+
+  it "redirected to that user's show page after click" do
+    click_link @user1.name
+    expect(page).to have_current_path(user_path(@user1.id))
   end
   it 'assigns all users to @users' do
-    get users_path
-    expect(assigns(:users)).to eq(User.all)
+    User.all.each do |user|
+      expect(page).to have_content(user.name)
+    end
   end
   it 'renders the name of the users' do
-    get users_path
-    expect(response.body).to include('Tom', 'Lilly')
+    expect(page.body).to include('Tom', 'Lilly')
   end
   it 'shows number of posts of user' do
-    get users_path
-    expect(response.body).to include('<p>Number of posts: 2</p>')
+    expect(page.body).to include('<p>Number of posts: 2</p>')
   end
   it 'renders the profile picture of the users' do
-    get users_path
-    expect(response.body).to include('img')
+    expect(page.body).to include('img')
   end
 end
