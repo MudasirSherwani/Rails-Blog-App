4d8e9443b8282b9c6b7ac7fd61e6d52893741598
Dell Pc <mudasir9757@gmail.com>
2023-04-12 17:36:10 +0500

    fix n+1 problem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
   def index
     user_id = params[:user_id]
-    @user = User.find(user_id)
-    @posts = @user.posts.order(created_at: :desc)
+    @user  = User.includes([:posts]).find(params[:user_id])
+    @posts = @user.posts.includes([:comments]).order(created_at: :desc)
   end
 
   def create
--- app/models/post.rb
+++ app/models/post.rb
@@ -13,6 +13,6 @@ class Post < ApplicationRecord
   end
 
   def most_recent_comments
-    comments.order(created_at: :desc).limit(5)
+    comments.includes(:author).order(created_at: :desc).limit(5)
   end
 end
