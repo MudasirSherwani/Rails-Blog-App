a8e77870859d1ed6179cf9bd0974b2fa9419a5ba
Dell Pc <mudasir9757@gmail.com>
2023-04-13 14:56:43 +0500

    Add delete method to comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,10 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.author = current_user
+    if @user.id != current_user.id
+      flash[:alert] = 'You do not have permission to add comment'
+      redirect_to root_path and return
+    end
     @comment.posts = Post.find(params[:post_id])
     if @comment.save
       redirect_to user_post_path(author_id: params[:author_id], id: params[:post_id])
@@ -15,6 +19,15 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @post = @comment.post
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to user_post_path(@post), notice: 'Comment deleted successfully.' }
+      format.json { head :no_content }
+    end
+  end
   private
 
   def comment_params
