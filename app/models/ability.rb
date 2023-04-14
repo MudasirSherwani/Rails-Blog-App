dde7a3c68aa66df5d0510ef6755f5b61ae46ed91
Dell Pc <mudasir9757@gmail.com>
2023-04-14 20:36:45 +0500

    Required Changes 2 done

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,22 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new
-    can :read, Post
     if user.role == 'admin'
       can :manage, :all
-    else
-      can :create, Post do |post|
-        post.author == user
-      end
-
-      can :destroy, Post do |post|
-        post.author == user
-      end
-      can :create, Comment
-      can :destroy, Comment do |comm|
-        comm.author == user
-      end
+    elsif user
+      can :manage, Post, author_id: user.id
+      can :manage, Comment, author_id: user.id
+      can :read, :all
     end
   end
 end
